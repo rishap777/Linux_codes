43a7e2338a840d290fda121a676231177582b02e
rishap777 <rishapsingh77@gmail.com>
2020-07-28 14:41:22 +0530

    yet to be programmed

---
+++ Thread_programs/threads_observe.c
@@ -0,0 +1,36 @@
+/* threads_observe.c creates a five threads.
+** These threads does nothing just looping in a while.
+** Hence we can the use ps,Top and other utilities to observer the behavior of
+** the threads by chanhing its attributes.
+** AUTHOR : Rishap Singh
+*/
+
+#include <pthread.h>
+#include <stdio.h>
+
+typedef struct threads_parameter
+{
+  int value;
+  char msg_from_main[128];
+}thread_param_t;
+
+
+/*Return type and the parameter type is important here do follow*/
+void *thread_func(void *param){
+  while (1) {
+    /* looping*/
+  }
+}
+
+int main(int argc, char const *argv[]) {
+  pthread_t thread1,thread2,thread3,thread4,thread5;
+  
+  thread_param_t *parm_t1;
+  thread_param_t parm_t2{.value=2,.msg_};
+  
+  pthread_create(&thread1,);
+
+
+
+  return 0;
+}
