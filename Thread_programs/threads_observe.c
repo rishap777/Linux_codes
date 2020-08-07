75e81f74fe60baa3cad8994399598e8dcca07ad0
Rishap Singh Rambahadur <rishapsingh704@gmail.com>
2020-08-07 20:36:15 +0530

    threads_observe.c has 2 threads created and parameter is passed via the main thread.
    Parameter passed has local scope of main thread i.e
    paasing the address of the local data declared and initilised in main thread
    local scope.

--- Thread_programs/threads_observe.c
+++ Thread_programs/threads_observe.c
@@ -7,6 +7,8 @@
 
 #include <pthread.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
 typedef struct threads_parameter
 {
@@ -17,6 +19,9 @@ typedef struct threads_parameter
 
 /*Return type and the parameter type is important here do follow*/
 void *thread_func(void *param){
+  thread_param_t *my_param =(thread_param_t *)param;
+  printf("my value=%d ",my_param->value);
+  printf("MSG from MAIN= %s ",my_param->msg_from_main);
   while (1) {
     /* looping*/
   }
@@ -26,11 +31,25 @@ int main(int argc, char const *argv[]) {
   pthread_t thread1,thread2,thread3,thread4,thread5;
   
   thread_param_t *parm_t1;
-  thread_param_t parm_t2{.value=2,.msg_};
+  thread_param_t parm_t2 = {.value=2,.msg_from_main="You are Thread 2 \n "};
+
+  parm_t1 = (thread_param_t *)malloc( sizeof(struct threads_parameter));
+  parm_t1->value=1;
+  strcpy(parm_t1->msg_from_main,"You are thread 1 \n ");
+
+  pthread_attr_t thread1_attr,thread2_attr;
+ 
+  pthread_attr_init(&thread1_attr);
+  pthread_attr_init(&thread2_attr);
   
-  pthread_create(&thread1,);
-
-
+  pthread_create(&thread1,&thread1_attr,thread_func,(void *)parm_t1);
+  pthread_create(&thread2, &thread2_attr , thread_func ,(void *)&parm_t2);
+  void *retval;
+  
+  pthread_join(thread1,&retval);
+  pthread_join(thread2,&retval);
+  
+  printf("\n all threads have exited \n");
 
   return 0;
-}
+}       
